e1135ef214f99b62a12628824935bd4150f461ea
Asaad <asaad1793.z@gmail.com>
2025-07-20 01:39:58 +0300

    add

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,7 @@
 books_by_author = Book.objects.filter(author='John Doe')
 books_ordered = Book.objects.order_by('title')
-library__name = 'city library'
 try:
-    librarian = Librarian.objects.get(library__name=library_name)
+    librarian = Librarian.objects.get(name=library_name)
     print(f"\nLibrarian for {library_name}: {librarian.name}")
 except Librarian.DoesNotExist:
     print(f"No librarian found for library: {library_name}")
