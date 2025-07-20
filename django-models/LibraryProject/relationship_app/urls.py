1a2d36d2fe50eb9a440ad4b0cad574d1ebaf4f23
Asaad <asaad1793.z@gmail.com>
2025-07-20 06:16:28 +0300

    add

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from .views import list_books, LibraryDetailView
+from django.urls import path, include
+from .views import list_books, LibraryDetailView, SignUpView
 
 urlpatterns = [
     path('books/', list_books, name='book_list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    
+
+    # auth URLs
+    path('accounts/', include('django.contrib.auth.urls')),  # login, logout, password_reset...
+    path('signup/', views.SignUpView.as_view(), name='signup'),  # view للتسجيل
+]
+
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,7 +5,7 @@ from .models import Library
 from django.views.generic.detail import DetailView
 
 # Function-based view to list all books
-def all_books_view(request):
+def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
@@ -24,3 +24,14 @@ class LibraryDetailView(DetailView):
   {% endfor %}
 </ul>
 '''
+#_____________
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')  # بعد التسجيل يوجه المستخدم لصفحة تسجيل الدخول
+    template_name = 'registration/signup.html'
+
+
