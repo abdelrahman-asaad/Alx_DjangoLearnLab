cd83681532f79cd66b3fcd0f0f8c13c0417a51fd
Asaad <asaad1793.z@gmail.com>
2025-07-20 07:38:43 +0300

    add

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -45,3 +45,33 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+
+## relationship_app/signals.py
+
+#from django.db.models.signals import post_save
+#from django.contrib.auth.models import User
+#from django.dispatch import receiver
+#from .models import UserProfile
+
+#@receiver(post_save, sender=User)
+#def create_user_profile(sender, instance, created, **kwargs):
+#    if created:
+#        UserProfile.objects.create(user=instance)
+
+## relationship_app/apps.py
+
+#from django.apps import AppConfig
+
+#class RelationshipAppConfig(AppConfig):
+#    default_auto_field = 'django.db.models.BigAutoField'
+#    name = 'relationship_app'
+
+#    def ready(self):
+#        import relationship_app.signals  # ← دا اللي بيفعل الإشارة
+
+
+##INSTALLED_APPS = [
+    
+#    'relationship_app.apps.RelationshipAppConfig',
+    
+#]
