ef8dd736c512f3f16693588d9c267571cb92493f
Asaad <asaad1793.z@gmail.com>
2025-07-26 06:40:58 +0300

    add

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -7,3 +7,23 @@ class BookAdmin(admin.ModelAdmin):
     search_fields = ('title', 'author')
 
 admin.site.register(Book, BookAdmin)
+
+#____week11____
+from django.contrib.auth.admin import UserAdmin #the default used class to represent 'user' in admin and manage users and their permissions
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['username', 'email', 'date_of_birth', 'is_staff'] #the columns appearing in users list in admin interface
+    fieldsets = UserAdmin.fieldsets + (         #fieldsets is an attritbtute that controls appears when modifying
+        ('معلومات إضافية', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + ( #add_field is an attritbtute that controls appears when adding new user
+        ('معلومات إضافية', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,21 +1,3 @@
 
 # Register your models here.
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin #the default used class to represent 'user' in admin and manage users and their permissions
-from .models import CustomUser
-
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff'] #the columns appearing in users list in admin interface
-    fieldsets = UserAdmin.fieldsets + (         #fieldsets is an attritbtute that controls appears when modifying
-        ('معلومات إضافية', {
-            'fields': ('date_of_birth', 'profile_photo'),
-        }),
-    )
-    add_fieldsets = UserAdmin.add_fieldsets + ( #add_field is an attritbtute that controls appears when adding new user
-        ('معلومات إضافية', {
-            'fields': ('date_of_birth', 'profile_photo'),
-        }),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
