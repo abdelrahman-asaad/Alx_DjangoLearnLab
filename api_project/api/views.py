5636e86b57fa3f222c49cdac719e4144cc938ec3
Asaad <asaad1793.z@gmail.com>
2025-08-02 19:42:31 +0300

    add

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
