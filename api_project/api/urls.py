350e731c7806358433652333a5b48e14cb0e5597
Asaad <asaad1793.z@gmail.com>
2025-08-02 20:52:57 +0300

    add

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -18,3 +18,20 @@ urlpatterns = [
     path('api/', include(router.urls)),
 path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
 ]
+#___________________________
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
+    path('api/', include(router.urls)),
+    path('books/', BookList.as_view(), name='book-list'),  # Maps to the BookList view
+]
+# This will create an endpoint at /api/token/ that you can use to obtain an authentication token.
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -17,3 +17,13 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+#_________________
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # أو IsAdminUser لو عايز تقصره على الأدمن
