3df08f80841212a6f38b962d74a4198effc59491
Asaad <asaad1793.z@gmail.com>
2025-08-02 20:17:51 +0300

    add

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -5,3 +5,15 @@ urlpatterns = [
 ]
 # endpoint for listing all books is /api/books/
 # This will allow you to access the list of books via the URL /api/books/
+
+#___________
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
