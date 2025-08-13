1a997c10c4840ce2ba4b408f59c8104d80a07e4b
Asaad <asaad1793.z@gmail.com>
2025-08-13 17:51:50 +0300

    add

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,19 +1,15 @@
-# Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Post, Comment
 from django.contrib import messages
+from .models import Post, Comment
 
-
-
-# 1- تسجيل مستخدم جديد
+# Your existing views stay the same
 def register(request):
-    # Import here to avoid circular import
     from .forms import CustomUserCreationForm
     
     if request.method == 'POST':
@@ -21,83 +17,72 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, 'Registration successful!')
             return redirect('profile')
     else:
         form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
 
-
-# 2- صفحة البروفايل
 @login_required
 def profile(request):
     return render(request, 'blog/profile.html')
 
-
-# 3- تسجيل دخول
 class CustomLoginView(LoginView):
     template_name = 'blog/login.html'
     redirect_authenticated_user = True
     
     def get_success_url(self):
+        messages.success(self.request, 'Login successful!')
         return reverse_lazy('profile')
 
-
-# 4- تسجيل خروج
 class CustomLogoutView(LogoutView):
     template_name = 'blog/logout.html'
-    next_page = 'post-list'  # Redirect to home after logout
-    http_method_names = ['get', 'post']
+    next_page = 'post-list'
 
-#http_method_names = ['get', 'post']  # Allow both GET and POST requests
-
-# عرض جميع التدوينات
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     ordering = ['-published_date']
-    paginate_by = 5  # Optional: add pagination
+    paginate_by = 5
 
-
-# عرض تفاصيل تدوينة
+# UPDATED: Enhanced PostDetailView with comments
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add comments and comment form to context
+        from .forms import CommentForm
+        context['comments'] = self.object.comments.all()
+        context['comment_form'] = CommentForm()
+        return context
 
-
-# إنشاء تدوينة جديدة - FIXED!
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content']  # Use fields instead of form_class
+    fields = ['title', 'content']
     
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, 'Post created successfully!')
         return super().form_valid(form)
-    
-    def get_success_url(self):
-        return reverse_lazy('post-detail', kwargs={'pk': self.object.pk})
 
-
-# تعديل تدوينة - FIXED!
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'blog/post_form.html'
-    fields = ['title', 'content']  # Use fields instead of form_class
+    fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        messages.success(self.request, 'Post updated successfully!')
         return super().form_valid(form)
 
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author
-    
-    def get_success_url(self):
-        return reverse_lazy('post-detail', kwargs={'pk': self.object.pk})
 
-
-# حذف تدوينة
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
@@ -107,49 +92,47 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post = self.get_object()
         return self.request.user == post.author
     
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, 'Post deleted successfully!')
+        return super().delete(request, *args, **kwargs)
 
-    # NEW: Comment Views
-@login_required
-def add_comment(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    
-    if request.method == 'POST':
-        from .forms import CommentForm
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            messages.success(request, 'Comment added successfully!')
-            return redirect('post-detail', pk=post.pk)
-    
-    return redirect('post-detail', pk=post.pk)
+# إنشاء تعليق
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    template_name = 'blog/comment_form.html'
+    fields = ['content']
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+
+# تعديل تعليق
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     template_name = 'blog/comment_form.html'
     fields = ['content']
-    
+
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.author
-    
-    def form_valid(self, form):
-        messages.success(self.request, 'Comment updated successfully!')
-        return super().form_valid(form)
 
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
+
+# حذف تعليق
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
-    
+
     def test_func(self):
         comment = self.get_object()
         return self.request.user == comment.author
-    
+
     def get_success_url(self):
         return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
-    
-    def delete(self, request, *args, **kwargs):
-        messages.success(request, 'Comment deleted successfully!')
-        return super().delete(request, *args, **kwargs)
